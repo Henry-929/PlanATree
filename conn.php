9ad4b8d7f33f23ae92c3b35a993cbc710e3dd759
Bin_Jiang <40458689@QQ.com>
2019-09-07 15:17:27 +1200

    change to use heroku databse

--- conn.php
+++ conn.php
@@ -1,10 +1,11 @@
 
 <?php
+//bfe9fe7d7536e5:d922c313@us-cdbr-iron-east-02.cleardb.net/heroku_4a3b3456e356787?reconnect=true
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "se";
+$servername = "us-cdbr-iron-east-02.cleardb.net";
+$username = "bfe9fe7d7536e5";
+$password = "d922c313";
+$dbname = "heroku_4a3b3456e356787";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
