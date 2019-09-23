fcc6ae64d44c1c7af74d8a8eab5250595699b795
HareemKhan <hareemkhan@hotmail.co.nz>
2019-09-24 02:28:47 +1200

    adding the old working code

--- ChatList.php
+++ ChatList.php
@@ -130,21 +130,18 @@ $listed=array();
 $message=array();
 $count=0;
 $notification=0;
-$seenid=array();
-$username1seen=array();
-$username1WITHseen=array();
+
 while($rows=mysqli_fetch_assoc($r2)){
 	$array[]=$rows['id'];
 }
-$q2='SELECT user_id, user_name1 FROM `personal_message` ';
-//$q1='SELECT * FROM `personal_message` WHERE  `Seen_Status`!="seen" AND Reply!="YES"';
-$q3='SELECT * FROM `personal_message` WHERE  `Seen_Status`="seen" ';
+//$q2='SELECT user_id, user_name1 FROM `personal_message` ';
+$q1='SELECT * FROM `personal_message` WHERE  `Seen_Status`!="seen" AND Reply!="YES"';
+
 $r1= mysqli_query($conn,$q1);
-$r4= mysqli_query($conn,$q2);
-$r3= mysqli_query($conn,$q3);
+
 
 if ($r2->num_rows >0){
-	while($row=mysqli_fetch_assoc($r4)){
+	while($row=mysqli_fetch_assoc($r1)){
 		$id[]=$row['user_id'];
 		$username1[]=$row['user_name1'];
 		}
