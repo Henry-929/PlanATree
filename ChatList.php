2e1b41a685746bf478271801384ce06e572ff8bd
HareemKhan <hareemkhan@hotmail.co.nz>
2019-09-24 01:08:52 +1200

    Removed one if statement from the unseen users while loop

--- ChatList.php
+++ ChatList.php
@@ -198,14 +198,14 @@ if ($r2->num_rows >0){
 					
 					if(!(in_array($seenid[$i],$listed))){
 			
-					if(!(in_array($username1seen[$i],$username1WITHseen))){
+					
 					echo "
 				  <li class='list-group-item d-flex justify-content-between align-items-center'>
 					<h4><a class='stretched-link text-decoration-none' href='personal.php?username=".$username1seen[$i]."'>".$username1seen[$i]."</a></h4><br>
 					<span class='badge badge-primary badge-pill'>0</span>
 				  </li>";
 					$username1WITHseen[$i]=$username1seen[$i];		
-					}}
+					}
 		}
 		
 	
