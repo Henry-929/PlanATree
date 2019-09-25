fca1582c47b4852f403c831ed2ed4b65f8a5b172
Henry Zhang <770204042z@gmail.com>
2019-09-25 10:00:06 +0800

    Added new trees and hedges.

--- index.php
+++ index.php
@@ -134,6 +134,7 @@ session_start();
 				<li data-target="#carouselIndicators" data-slide-to="1"></li>
 				<li data-target="#carouselIndicators" data-slide-to="2"></li>
 				<li data-target="#carouselIndicators" data-slide-to="3"></li>
+				<li data-target="#carouselIndicators" data-slide-to="4"></li>
 			  </ol>
 			  <div class="carousel-inner">
 				<div class="carousel-item active">
@@ -147,6 +148,9 @@ session_start();
 				</div>
 				<div class="carousel-item">
 				  <img src="img/blueberry_dis.jpg" class="d-block w-100" alt="blueberry">
+				</div>
+				<div class="carousel-item">
+				  <img src="img/camelliacurly_dis.jpg" class="d-block w-100" alt="camelliacurly">
 				</div>
 			  </div>
 			  <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
